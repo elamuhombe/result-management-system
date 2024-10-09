44e1d5f07ca101a0ee11d0c0e668f4d80cfde035
Elaine Muhombe <emuhombe@gmail.com>
2024-10-09 17:54:59 +0300

    feat: add timestamps to Student model and rename to StudentModel

--- server/src/models/Students.ts
+++ server/src/models/Students.ts
@@ -3,14 +3,17 @@ import { Document, model, Schema } from 'mongoose';
 import { IStudent } from '../types/types';
 
 // Extend Document to include the IStudent interface
-const StudentSchema = new Schema<IStudent & Document>({
-  studentId: { type: String, required: true },
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, required: true, unique: true, match: /.+@.+\..+/ }, // Basic email format validation
-  results: { type: [String], default: [] }
-});
+const StudentSchema = new Schema<IStudent & Document>(
+  {
+    studentId: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true, match: /.+@.+\..+/ }, // Basic email format validation
+    results: { type: [String], default: [] }
+  },
+  { timestamps: true } // Enable automatic timestamps
+);
 
 // Export the Student model
-const Student = model<IStudent & Document>('Student', StudentSchema);
-export default Student;
+const StudentModel = model<IStudent & Document>('Student', StudentSchema);
+export default StudentModel;
