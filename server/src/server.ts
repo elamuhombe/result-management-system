f77dcd1585ae9e4e3ba551e1c54efc196e40594c
Elaine Muhombe <emuhombe@gmail.com>
2024-10-09 17:09:39 +0300

    feat: implement database connection logic to use MongoDB URI from environment variables. - Implemented promise-based connection handling with success and error logging.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,27 @@
-//src/server.ts
+// src/server.ts
+import { config } from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
+
+// Load environment variables from .env file
+config();
 
 const app = express();
 const port = 5300;
 
+// Get MongoDB URI from environment variables
+const db_uri = process.env.MONGODB_URI;
+
+// Connect to MongoDB
+mongoose.connect(db_uri!)
+  .then(() => {
+    console.log('Connected to database successfully');
+  })
+  .catch((error) => {
+    console.error('Problem connecting to database:', error);
+  });
+
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-
-
-
-
