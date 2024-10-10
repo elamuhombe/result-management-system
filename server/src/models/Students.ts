6096a1f8b69000a6e50c6717e49ba6abaaf31442
Elaine Muhombe <emuhombe@gmail.com>
2024-10-10 10:54:00 +0300

    refactor: change studentId type from String to ObjectId in Student schema

--- server/src/models/Students.ts
+++ server/src/models/Students.ts
@@ -1,19 +1,19 @@
 // src/models/Students.ts
-import { Document, model, Schema } from 'mongoose';
-import { IStudent } from '../types/types';
+import { Document, model, Schema } from "mongoose";
+import { IStudent } from "../types/types";
 
 // Extend Document to include the IStudent interface
 const StudentSchema = new Schema<IStudent & Document>(
   {
-    studentId: { type: String, required: true },
+    studentId: { type: Schema.Types.ObjectId, required: true },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true, match: /.+@.+\..+/ }, // Basic email format validation
-    results: { type: [String], default: [] }
+    results: { type: [String], default: [] },
   },
   { timestamps: true } // Enable automatic timestamps
 );
 
 // Export the Student model
-const StudentModel = model<IStudent & Document>('Student', StudentSchema);
+const StudentModel = model<IStudent & Document>("Student", StudentSchema);
 export default StudentModel;
