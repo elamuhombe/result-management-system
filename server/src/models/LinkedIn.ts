d7527469aba51dd1b4096baee597268708b0aa2f
Elaine Muhombe <emuhombe@gmail.com>
2024-10-10 11:16:09 +0300

    feat: create LinkedIn post mark model
    - Added LinkedInSchema for managing LinkedIn post marks.
    - Fields include studentId, postId, score, and engagementMetrics (likes, comments, shares).
    - Implemented timestamps for tracking creation and updates.

---
+++ server/src/models/LinkedIn.ts
@@ -0,0 +1,20 @@
+//src/models/LinkedIn.ts
+
+import { model, Schema, SchemaType } from "mongoose";
+import { ILinkedInPostMark } from "../types/types";
+
+const LinkedInSchema: Schema<ILinkedInPostMark & Document> = new Schema({
+    studentId: {type: Schema.ObjectId, unique:true, required: true, ref: "Students"},
+    postId: {type: String, unique: true, required: true},
+    score:{type: Number, required: true},
+    engagementMetrics: {
+        likes: { type: Number, default: 0 },
+        comments: { type: Number, default: 0 },
+        shares: { type: Number, default: 0 },
+      },
+},{timestamps: true})
+
+const LinkeInModel = model <ILinkedInPostMark & Document>("LinkedIn", LinkedInSchema
+)
+
+export default LinkeInModel
