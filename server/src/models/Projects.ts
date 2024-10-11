1a8a99e0dde8f8fea23afe76976d0f72c142f3d7
Elaine Muhombe <emuhombe@gmail.com>
2024-10-11 17:53:59 +0300

    chore: add  and  to

--- server/src/models/Projects.ts
+++ server/src/models/Projects.ts
@@ -1,19 +1,19 @@
-//src/models/Projects.ts
+// src/models/Project.ts
 
 import mongoose, { model, Schema } from "mongoose";
 import { IProject } from "../types/types";
 
-const ProjectSchema: Schema<IProject & Document> = new Schema(
-  {
-    projectId: { type: String, required: true, unique: true },
-    studentId:{  type: mongoose.Schema.Types.ObjectId, required: true, ref: "Students"},
-    project_title: { type: String, required: true },
-    submission_date: { type: Date, required: true },
-    marks_received: { type: Number, required: true },
-  },
-  { timestamps: true }
-);
+// Define the schema for Project
+const ProjectSchema: Schema<IProject & Document> = new Schema({
+    studentId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Students" }, // Reference to the Student model
+    project_title: { type: String, required: true }, // Title of the project
+    submission_date: { type: Date, required: true }, // Date of submission
+    review_score: { type: Number, required: true }, // Marks received for the project review
+    submission_score: { type: Number, required: true }, // Marks received for the project submission
+    maximum_score: { type: Number, required: true }, // Maximum score possible
+}, { timestamps: true }); // Automatically manage createdAt and updatedAt timestamps
 
+// Export the Project model
 const ProjectModel = model<IProject & Document>("Project", ProjectSchema);
 
 export default ProjectModel;
