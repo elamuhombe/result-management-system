f4331fa0b60a07bbbc146636d424e268ff51154e
Elaine Muhombe <emuhombe@gmail.com>
2024-10-12 18:34:56 +0300

    feat: implement student data validation in addStudent method;

--- server/src/validators/StudentSchema.ts
+++ server/src/validators/StudentSchema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 
 // Define the validation schema for the IStudent interface
 export const studentSchema = z.object({
-  studentId: z.string().nonempty("Student ID is required"),
+  uniqueStudentId: z.string().nonempty("Student ID is required"),
   firstName: z.string().nonempty("First name is required"),
   lastName: z.string().nonempty("Last name is required"),
   email: z.string().email("Invalid email address"), // Validates email format
