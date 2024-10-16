0f54fd926f87f7b3963d74f580e39e1e6729e092
Elaine Muhombe <emuhombe@gmail.com>
2024-10-16 20:01:02 +0300

    feat: Create ResultService with methods for calculating total marks and fetching results

---
+++ server/src/services/ResultService.ts
@@ -0,0 +1,35 @@
+// services/ResultService.ts
+
+import { IResult } from "../types/types";
+import ResultRepository from "./repositories/ResultRepository";
+
+
+
+
+class ResultService {
+  // Other existing methods...
+
+  calculateTotalMarks(result: IResult): number {
+    return (
+      result.attendance_score +
+      result.project_review_score +
+      result.assessment_score +
+      result.project_submission_score +
+      result.linkedin_score
+    );
+  }
+
+  async getResultWithTotalMarks(uniqueStudentId: string): Promise<any> {
+    const result = await ResultRepository.findResultById(uniqueStudentId);
+    if (!result) {
+      return null;
+    }
+    const totalMarks = this.calculateTotalMarks(result);
+    return {
+      ...result,
+      totalMarks, // Add totalMarks to the returned object
+    };
+  }
+}
+
+export default new ResultService();
