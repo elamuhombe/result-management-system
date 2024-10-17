1046a82ee9335191ebf44c96c86811b472e9011b
Elaine Muhombe <emuhombe@gmail.com>
2024-10-17 07:44:40 +0300

    Refactor AuthService: Integrated AuthRepository for user registration and retrieval, added error handling for duplicate users

--- server/src/services/AuthService.ts
+++ server/src/services/AuthService.ts
@@ -1,66 +1,30 @@
-//src/services/AuthService.ts
-import { JsonWebTokenError } from "jsonwebtoken";
-import { UserModel } from "../models";
+// src/services/AuthService.ts
 import { IUser } from "../types/types";
-import * as bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import { UserSchema } from "../validators/UserSchema";
+import AuthRepository from "./repositories/AuthRepository";
 
 class AuthService {
-  //register a new user
-  async registerUser(userData: IUser): Promise<IUser> {
-      // Validate request body
-      const validatedData = UserSchema.parse(userData)
+  private authRepository: AuthRepository;
 
-    const { username, password, role } = validatedData;
-
-    // check if user already exists
-    const existingUser = await UserModel.findOne({ username });
-
-    // throw an error if user already exists
-    if (existingUser) {
-      throw new Error("username already exists");
-    }
-
-    // hash the password
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    // create a new user
-    const newUser = new UserModel({
-      username,
-      password: hashedPassword,
-      role,
-    });
-
-    return await newUser.save();
+  constructor() {
+    this.authRepository = new AuthRepository();
   }
-  // login an existing user
-  async login(
-    username: string,
-    password: string
-  ): Promise<{ user: Partial<IUser>; token: string }> {
-    //find the user by username
-    const user = await UserModel.findOne({ username });
 
-    if (!user) {
-      throw new Error("inavlid username");
-    }
-    // compare the password
-    const validPassword = await bcrypt.compare(password, user.password);
-    if (!validPassword) {
-      throw new Error("invalid password");
-    }
-
-    // Generate a JWT token
-    const token = jwt.sign(
-      { username: user.username, role: user.role },
-      process.env.JWT_SECRET || "your_jwt_secret",
-      {
-        expiresIn: "1h", // Token expiration time
-      }
+  // Method to register a new user
+  async registerUser(userData: Partial<IUser>): Promise<IUser | null> {
+    const existingUser = await this.authRepository.findUserByUsername(
+      userData.username || ""
     );
+    if (existingUser) {
+      throw new Error("User already exists");
+    }
 
-    // return only essential user information
-    return { user: { username: user.username, role: user.role }, token };
+    return await this.authRepository.createUser(userData);
+  }
+
+  // Method to find user by username
+  async getUserByUsername(username: string): Promise<IUser | null> {
+    return await this.authRepository.findUserByUsername(username);
   }
 }
+
+export default AuthService;
